d4beda6a0e280e36c25cbe95240f12888d8eaa64
aiymfine <147755224+Aiymfine@users.noreply.github.com>
2025-04-14 12:20:20 +0500

    update

--- ChainofResponsibility/SupportHandler.java
+++ ChainofResponsibility/SupportHandler.java
@@ -1,20 +1,8 @@
 abstract class SupportHandler{
-    public static int LOW=1;
-    public static int MEDIUM=2;
-    public static int HIGH=3;
-
-    protected int level;
-    protected SupportHandler nextHandler;
-    public SupportHandler setNextHandler(SupportHandler nextHandler){
-        this.nextHandler= nextHandler;
-        return nextHandler;
+    protected SupportHandler next;
+    public SupportHandler setNext(SupportHandler next){
+        this.next=next;
+        return next;
     }
-    public void handleRequest(int level, String issue){
-        if(this.level== level){
-            process(issue);
-        }
-        if(nextHandler !=null){
-            nextHandler.handleRequest(level, issue);
-        }
-    }   protected abstract void process(String issue);
+    public abstract void handle(String issue);
 }
